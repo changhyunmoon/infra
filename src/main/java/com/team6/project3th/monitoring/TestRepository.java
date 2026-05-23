34d228e689b50a01d4cedc2fb5302fe905db0d59
changhyunmoon <67505983+changhyunmoon@users.noreply.github.com>
2026-05-23 15:22:07 +0900

    Feat#10: CI 구축

--- src/main/java/com/team6/project3th/monitoring/TestRepository.java
+++
@@ -1,6 +0,0 @@
-package com.team6.project3th.monitoring;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<TestEntity, Long> {
-}
